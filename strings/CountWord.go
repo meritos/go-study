dac0d3dfd0901db3a263fa6f9566d2252612d208
merito-work <meritos@protonmail.com>
2017-08-08 02:08:45 +0300

    Add strings sample

---
+++ strings/CountWord.go
@@ -0,0 +1,90 @@
+package strings
+
+import (
+
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
+func cheet(){
+	fmt.Print("");
+}
+
+/**
+ * CountWord2(in string) string
+ *
+ *    ...
+ */
+func CountWord2(in string) string {
+
+	in = strings.Trim(in, " ")
+
+	words  := strings.Split(in, " ")
+	result := make([]byte, len(in) + calcAddition(in))
+
+
+ 	copyIdx := 0;
+	for _, word := range words {
+
+		// *** ver 1 ***
+		//newWord := strings.Join([]string{word, strconv.Itoa(len(word)), " "}, "")
+		//copy(result[copyIdx:], newWord)
+		//copyIdx += len(newWord)
+
+		// *** ver 2 ***
+		copy(result[copyIdx:], word)
+		copyIdx += len(word)
+
+		wordLenStr := strconv.Itoa(len(word))
+		copy(result[copyIdx:], wordLenStr)
+		copyIdx += len(wordLenStr)
+
+		if copyIdx + 1 < len(result){
+
+			 copy(result[copyIdx:], " ")
+			 copyIdx++
+		}
+
+	}
+
+	return string(result)
+
+}
+
+
+/**
+ * calcAddition(in string) int
+ *
+ *     ...
+ *
+ */
+func calcAddition(in string) int {
+
+		// counts spaces in the string 
+		bufferSizeAdd := 0;
+		wordLenCount  := 0;
+		for idx, elem := range in {
+    
+			if elem == 32 || idx == len(in) - 1 {
+
+				bufferSizeAdd++
+
+				if wordLenCount > 9 {
+					
+					bufferSizeAdd++
+				} else if wordLenCount > 99 {
+
+					bufferSizeAdd = bufferSizeAdd + 2;
+				}
+
+				wordLenCount = 0
+			}
+
+			wordLenCount++
+		}
+
+		return bufferSizeAdd;	
+}
+
---
+++ strings/CountWord_test.go
@@ -0,0 +1,72 @@
+
+package strings
+
+import (
+
+	"testing"
+)
+
+
+func TestCountWord2_1(t *testing.T){
+
+	expected := "Hello5 World5 Count5 My2 Words5 Long4 words5 also4 like4 Ronaldinhioooo14"
+	out := CountWord2("Hello World Count My Words Long words also like Ronaldinhioooo")
+
+	if out != expected {
+
+		t.Errorf("\n ERROR ... out:  %q, expected: %q", out, expected)
+	}
+}
+
+
+
+func TestCountWord2_2(t *testing.T){
+
+	expected := ""
+	out := CountWord2("")
+
+	if out != expected {
+
+		t.Errorf("\n ERROR ... out:  %q, expected: %q", out, expected)
+	} 
+}
+
+func TestCountWord2_3(t *testing.T){
+
+	expected := "oneWord7"
+	out := CountWord2("oneWord")
+
+	if out != expected {
+
+		t.Errorf("\n ERROR ... out:  %q, expected: %q", out, expected)
+	} 
+}
+
+func TestCountWord2_4(t *testing.T){
+
+	expected := "Ronaldinhioooo14"
+	out := CountWord2("Ronaldinhioooo")
+
+	if out != expected {
+
+		t.Errorf("\n ERROR ... out:  %q, expected: %q", out, expected)
+	} 
+}
+
+
+func BenchmarkCountWord2_4(b *testing.B){
+
+	b.ReportAllocs()
+
+	for i:=0; i < 1000000; i++ {
+
+		CountWord2("Hello World Count My Words Long words also like Ronaldinhioooo ")
+	}
+
+
+
+}
+
+
+
+
